6fde465b7973f9a5559c2352555784e4f3f3e4c9
Kyle Gaffney <kylebryangaffney@gmail.com>
2025-02-24 08:10:34 -1000

    Update Look And feel Groups

--- LookAndFeel.h
+++ LookAndFeel.h
@@ -66,6 +66,14 @@ public:
 
     juce::Font getLabelFont(juce::Label&) override;
 
+    juce::Label* createSliderTextBox(juce::Slider&) override;
+
+    void drawTextEditorOutline(juce::Graphics&, int, int, juce::TextEditor&) override
+    {
+    }
+
+    void fillTextEditorBackground(juce::Graphics&, int width, int height, juce::TextEditor&) override;
+
 private:
 
     juce::DropShadow dropShadow { Colors::Knob::dropShadow, 6, {0, 3} };
